aa40e9d5b3b9a9782bf16713cc1d1670806f97f9
Emily Schmitt <emschmitt@davidson.edu>
2021-10-25 13:56:20 -0400

    got the quotes to generate

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@
 /* javascript */
 let quotes = ['A most notable coward, an infinite and endless liar, an hourly promise breaker, the owner of no one good quality. All’s Well That Ends Well (Act 3, Scene 6)',
 'Away, you starvelling, you elf-skin, you dried neat’s-tongue, bull’s-pizzle, you stock-fish! Henry IV Part 1 (Act 2, Scene 4)' ,
-'Away, you three-inch fool! The Taming of the Shrew (Act 4, Scene 1)']
+'Away, you three-inch fool! The Taming of the Shrew (Act 4, Scene 1)'
+]
 
-console.log(quotes[0]);
+ function generateQuotes(){
+ let randomNumber = Math.floor(Math.random() * (quotes.length));
+ document.getElementById('quoteDisplay').innerHTML = quotes[randomNumber];
+ }
